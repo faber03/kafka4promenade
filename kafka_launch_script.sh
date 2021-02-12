94804f1f912a8f1bd63830b4bc5ac95a85387b6d
woland7 <antonio.deiasio@gmail.com>
2021-02-12 11:22:20 +0100

    added chart for the mongodb kafka connector

--- kafka_launch_script.sh
+++ kafka_launch_script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export TILLER_NAMESPACE=promenade
+
 oc delete pvc datadir-kafka-cp-zookeeper-2
 oc delete pvc datadir-kafka-cp-zookeeper-1
 oc delete pvc datadir-kafka-cp-zookeeper-0
@@ -14,11 +16,17 @@ helm del --purge kafka
 
 helm del --purge kafka-connect-artemis
 
+helm del --purge kafka-connect-neo4j
+
 
 helm install --name kafka .
 
 cd charts/cp-kafka-connect-artemis || exit
 
-sleep 120
+sleep 100
 
 helm install --name kafka-connect-artemis .
+
+cd ~/Desktop/Promenade/cp-kafka-helm-charts/charts/cp-kafka-connect-neo4j/ || exit
+
+helm install --name kafka-connect-neo4j .
